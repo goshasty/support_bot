4592581522a63d80a50c187bbe09426c4a7690f5
goshasty <goshasty@yandex.ru>
2018-02-09 16:31:24 +0300

    refactoring 1

---
+++ webhook.py
@@ -0,0 +1,27 @@
+import cherrypy
+
+
+'''
+Mihan' s code, I' ve no idea
+
+'''
+
+
+"""WEBHOOK CLASS"""
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                'content-type' in cherrypy.request.headers and \
+                cherrypy.request.headers['content-type'] == 'application/json':
+
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+"""END WEBHOOK CLASS. BELOW JUST YOUR CODE"""
