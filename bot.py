ed2ab4b1dcb2cc478f6ce741736767251cc9203c
goshasty <goshasty@yandex.ru>
2018-02-23 16:03:37 +0300

    with Dima

--- bot.py
+++ bot.py
@@ -10,22 +10,23 @@ from telebot import types
 import sqlite3
 import cherrypy
 
+from classSC import *
 import tokens
-import const
-from const import WEBHOOK_HOST, WEBHOOK_PORT, WEBHOOK_LISTEN, \
-                  WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV, \
-                  WEBHOOK_URL_BASE
+from const import *
 
 
 categories = []
+categories_id = []
+subcategories = []
+
 code_wrongs = {
-    -1: const.smth_wrong,
-    1: const.sc_is_empty,
-    2: const.no_category,
-    3: const.no_subcategory
+    -1: SMTH_WRONG,
+    1: SC_IS_EMPTY,
+    2: NO_CATEGORY,
+    3: NO_SUBCATEGORY
 }
 
-bot = telebot.TeleBot(tokens.token_dimas)
+bot = telebot.TeleBot(tokens.TOKEN_PRINCE)
 
 class WebhookServer(object):
     @cherrypy.expose
@@ -44,12 +45,13 @@ class WebhookServer(object):
 
 def main():
     load_categories()
+    load_subcategories()
     logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
                         level=logging.DEBUG, filename=u'YourSupportLogs.log')#!
     bot.remove_webhook()
     if ((len(sys.argv) < 2) or
             ((sys.argv[1] != 'dev') and (sys.argv[1] != 'deploy'))):
-        print("Specify starting mode (first arg) 'dev' or 'deploy'") //const
+        print("Specify starting mode (first arg) 'dev' or 'deploy'")
         sys.exit()
 
     if(sys.argv[1] == 'deploy'):
@@ -59,49 +61,172 @@ def main():
 
 def load_categories():
     """
-    Return list of categories from DB
+    Load Category objects from DB
 
     """
     print("Loading categories...")
-    con = sqlite3.connect('test.db')
-    cur = con.cursor()
-    cur.execute('SELECT name FROM categories')
-    categories_ = cur.fetchall()
-
-
-    #print("Loading superusers...")
-    #cur.execute('SELECT chat_id FROM superusers')
-    #users = cur.fetchall()
-    #con.close()
-    #for x in users:
-        #superusers.append(int(x[0]))
-
+    connectorDB = sqlite3.connect('test.db')
+    cursorDB = connectorDB.cursor()
+    cursorDB.execute('SELECT * FROM categories')
+    categories_ = cursorDB.fetchall()
+    connectorDB.close()
+    print(categories_)
     for n in categories_:
-        categories.append(n[0])
+        categories.append(n[1])
+        categories_id.append(n[0])
+    return
 
-#DO NOTHING
+def load_subcategories():
+    """
+    Load Subcategory objects from DB
+
+    """
+    connectorDB = sqlite3.connect('test.db')
+    cursorDB = connectorDB.cursor()
+    cursorDB.execute('SELECT * FROM subcategories')
+    subcategories_ = cursorDB.fetchall()
+    connectorDB.close()
+    print(subcategories_)
+    i = 0
+    for n in subcategories_:
+        sc = Subcategory(n[1], n[2], n[0])
+        subcategories.append(sc)
+    return
+
+@bot.message_handler(commands=["get"])
+def get_statistics(message):
+    """
+        To know your chat_id
+    """
+    print(str(message.chat.id))
+    if m.chat.id in superusers:
+        counts = get_visit_counts()
+        print(str(counts))
+        bot.send_message(m.chat.id, UNIQUE_USERS, UNIQUE_CLICKS  % ( ##
+                             counts[0], counts[1]))
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    """
+    Some actions with server
+    User will see buttons-categories
+
+    """
+    print("Start command")
+    check_users(message.chat.id)
+    msg = invalidate(message, INVITE_CATEGORY, categories)#start_const
+
+@bot.message_handler(content_types=["text"])
+def user_send_text(message):
+    """
+    User send message, check if it' s a category -> show subcategories
+                                      a subcategory -> show companies
+
+    """
+    categoryID = check_categories(message.text)
+    if(categoryID != 0):
+        currentSubcategories = choose_subcategory(categoryID)
+        msg = invalidate(message, INVITE_SUBCATEGORY,
+                        currentSubcategories, buttonBack = 1)
+        return
+    subcategoryID = check_subcategories(message.text)
+    if(subcategoryID != 0):
+         show_companies_in_subcategory(message, subcategoryID)
+         msg = invalidate(message, INVITE_CATEGORY, categories)
+         return
+
+    if(message.text == "<<"):
+        msg = invalidate(message, INVITE_CATEGORY, categories)
+        return
+    msg = invalidate(message, INVITE_CATEGORY, categories)
+
+def check_categories(text):
+    """
+    Return id of choosen category, if there' s no such category -> 0
+
+    """
+    i = 0 #
+    for c in categories:
+        if(c == text):
+            return categories_id[i]
+        i = i+1
+    return 0
+
+def choose_subcategory(id):
+    """
+    Return list of names subcategories, which belong to category with id
+
+    """
+    currentSubcategories = []
+    for sc in subcategories:
+        if(sc.check_mainID(id)):
+            currentSubcategories.append(sc.get_name())
+    return currentSubcategories
+
+def check_subcategories(text):
+    """
+    Return id of choosen subcategory, if there' s no such subcategory -> 0
+
+    """
+    for sc in subcategories:
+        if(sc.check_name(text) == True):
+            return(sc.get_id())
+    return 0
+
+def invalidate(m, text_mes, list, buttonBack=0):
+    """
+    Show a grid of buttons, which associated with elements of list
+    """
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    keyboard.add(*[types.KeyboardButton(n) for n in list])
+    if buttonBack == 1:
+    	keyboard.add(types.KeyboardButton('<<'))
+    msg = bot.send_message(m.chat.id, text_mes, reply_markup=keyboard)
+    return msg
+
+def show_companies_in_subcategory(message, subcategoryID):
+    """
+    User will see info about companies
+
+    """
+    list_of_companies = get_company_info(subcategoryID)
+    if not list_of_companies:
+        execuse_smth(message, code=1) ##
+        return None
+    for company in list_of_companies:
+        """
+        keyboard = types.InlineKeyboardMarkup()
+        text_url = "https://beautyhttp.herokuapp.com/redirect_user/"
+        text_url += str(m.chat.id) + "/"
+        text_url += company[5] + "/"
+        # print(text_url)
+        url_button = types.InlineKeyboardButton(
+            text="telegram канал", url=text_url)
+        keyboard.add(url_button)
+        """
+        bot.send_message(message.chat.id, company_out_text(company))
+
 def check_users(cur_id):
     print("Checking users...")
-    con = sqlite3.connect('test.db')
-    cur = con.cursor()
-    cur.execute('SELECT user_id FROM users')
-    all_users_ = cur.fetchall()
+    connectorDB = sqlite3.connect('test.db')
+    cursorDB = connectorDB.cursor()
+    cursorDB.execute('SELECT user_id FROM users')
+    all_users_ = cursorDB.fetchall()
     all_ussers = [int(x[0]) for x in all_users_]
     if not cur_id in all_ussers:
-        cur.execute("INSERT INTO 'users' ('user_id') VALUES (%d)" % cur_id)
-        con.commit()
-    con.close()
+        cursorDB.execute("INSERT INTO 'users' ('user_id') VALUES (%d)" % cur_id)
+        connectorDB.commit()
+    connectorDB.close()
 
-#DO NOTHING
 def get_visit_counts():
     print("Loading visits...")
-    con = sqlite3.connect('test.db')
-    cur = con.cursor()
-    cur.execute("SELECT COUNT(*) FROM users")
-    users_count = int(cur.fetchone()[0])
-    cur.execute("SELECT DISTINCT telegram FROM companies")
-    telegrams_ = cur.fetchall()
-    con.close()
+    connectorDB = sqlite3.connect('test.db')
+    cursorDB = connectorDB.cursor()
+    cursorDB.execute("SELECT COUNT(*) FROM users")
+    users_count = int(cursorDB.fetchone()[0])
+    cursorDB.execute("SELECT DISTINCT telegram FROM companies")
+    telegrams_ = cursorDB.fetchall()
+    connectorDB.close()
     telegrams = [str(x[0]) for x in telegrams_]
     http = urllib3.PoolManager()
     clicks_count = 0
@@ -111,178 +236,16 @@ def get_visit_counts():
         clicks_count += int(response.data)
     return [users_count, clicks_count]
 
-
-@bot.message_handler(content_types=["text"])
-def user_send_text(m): #вычесть команды
-    execuse_smth(m, code=2)
-
-@bot.message_handler(commands=["get"])
-def get_statistics(m):
-    """
-        To know your chat_id
-    """
-    print(str(m.chat.id))
-    if m.chat.id in const.superusers:
-        print("superuser")
-        counts = get_visit_counts()
-        print(str(counts))
-        bot.send_message(m.chat.id, unique_users, unique_clicks  % (
-                             counts[0], counts[1]))
-
-@bot.message_handler(commands=['start'])
-def start(m):
-    """
-    Some actions with server
-    User will see buttons-categories
-    Transfer control to choose_category (next handler)
-    """
-
-    print("Start command")
-    check_users(m.chat.id)
-
-    #http = urllib3.PoolManager()
-    #http.request('GET', 'https://beautyhttp.herokuapp.com', timeout=0.5)
-    #keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
-    print("start2")
-    msg = invalidate(m, const.invite_cat, categories,0)
-    bot.register_next_step_handler(msg, choose_category)
-
-def invalidate(m, text_mes, list, buttonBack):
-    """
-    Show a grid of buttons, which associated with elements of list
-
-    """
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    keyboard.add(*[types.KeyboardButton(n) for n in list])
-    if buttonBack == 1:
-    	keyboard.add(types.KeyboardButton('<<'))
-    msg = bot.send_message(m.chat.id, text_mes, reply_markup=keyboard)
-    return msg
-
-def choose_category(m):
-    """
-    User will see buttons-subcategories
-    Transfer control to choose_subcategory (next handler)
-
-    """
-    i = 1
-    print("category")
-    for name in categories:
-        if m.text == name and i == len(categories):
-            bot.send_message(m.chat.id, const.FAQ)
-            msg = invalidate(m, const.invite_cat, categories,0)
-            bot.register_next_step_handler(msg, choose_category)
-            return  # handling of FAQ
-        if m.text == name and i < len(categories):
-            sc = load_subcategories(i + 1)
-            if (sc == -1):
-                execuse_smth(m, code=-1)
-                return
-            if (sc == []):
-                execuse_smth(m, code=0)
-                return
-            msg = invalidate(m, const.invite_sc, sc, 1)
-            bot.register_next_step_handler(msg, choose_subcategory)
-            return
-        i += 1
-
-    if not m.text in const.available_commands:
-        execuse_smth(m, code=2)
-    return
-
-def load_subcategories(number_cat):
-    """
-    Load names of subcategories from DB after choosing category
-
-    """
-    try:
-        con = sqlite3.connect('test.db')
-        cur = con.cursor()
-        cur.execute('SELECT name FROM subcategories WHERE main_category_id = (?)', (number_cat,))
-        subcategories = cur.fetchall()
-        i = 0
-        for n in subcategories:
-            subcategories[i] = n[0]
-            i += 1
-        # print("suc")
-        con.close()
-    except Exception as e:
-        subcategories = -1
-        handling_error(e)
-
-    return subcategories
-
-def choose_subcategory(m):
-    """
-    User will see info about companies
-    Transfer control to choose_category
-
-    """
-    print("subcategory")
-    if m.text in const.available_commands:
-        return None
-
-    id = get_subcategory_id(m.text)
-    # print(id)
-    # print(m.text)
-    # print(const.available_commands)
-    if id == -1:
-        execuse_smth(m, code=3)
-        return None
-
-    list_of_companies = get_company_info(id)
-    # !! list_of_companies = get_company_info(id)
-
-    if not list_of_companies:
-        execuse_smth(m, code=1)
-        return None
-
-    for company in list_of_companies:
-        keyboard = types.InlineKeyboardMarkup()
-        text_url = "https://beautyhttp.herokuapp.com/redirect_user/"
-        text_url += str(m.chat.id) + "/"
-        text_url += company[5] + "/"
-        # print(text_url)
-        url_button = types.InlineKeyboardButton(
-            text="telegram канал", url=text_url)
-        keyboard.add(url_button)
-        bot.send_message(m.chat.id, company_out_text(company), reply_markup=keyboard)
-
-    msg = invalidate(m, const.invite_cat, categories,0)
-    bot.register_next_step_handler(msg, choose_category)
-    return None
-#DO NOTHING
-def get_subcategory_id(name):
-    """
-    Return id of choossing subcategory
-    """
-    try:
-        con = sqlite3.connect('test.db')
-        cur = con.cursor()
-        # print(name)
-        cur.execute('SELECT subcategory_id FROM subcategories WHERE name = (?)', [name])
-        res = cur.fetchall()
-        con.close()
-        # print(res)
-        if not res:
-            return -1
-        return res[0][0]
-
-    except Exception as e:
-        res = -1
-        handling_error(e)
-        return res
-#DO NOTHING BELOW
 def get_company_info(number_sc):
     """
     Return a list of companies which belongs definite subcategory
 
     """
-    con = sqlite3.connect('test.db')  # do try
-    cur = con.cursor()
-    cur.execute('SELECT * FROM companies WHERE main_subcategory_id = (?)', [number_sc])
-    res = cur.fetchall()
-    con.close()
+    connectorDB = sqlite3.connect('test.db')  # do try
+    cursorDB = connectorDB.cursor()
+    cursorDB.execute('SELECT * FROM companies WHERE main_subcategory_id = (?)', [number_sc])
+    res = cursorDB.fetchall()
+    connectorDB.close()
     return res
 
 def company_out_text(text):
@@ -292,7 +255,8 @@ def company_out_text(text):
     """
     out_t = text[2] + "\n"  # name
     out_t += text[3] + "\n"  # description
-    out_t += "Рабочий телефон \n" + text[4]
+    out_t += "Рабочий телефон \n" + text[4] + "\n"
+    out_t += "@your_support_bot"
     return out_t
 
 def handling_error(e):
@@ -301,30 +265,27 @@ def handling_error(e):
 
     """
     # print(e)
-    bot.send_message(const.my_chat, e)
+    bot.send_message(my_chat, e)
     t = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
     f = open('errors.txt', 'a')
     text = str(e) + '\n' + t + '\n'
     f.write(text)
     f.close()
-    bot.send_message(const.my_chat, t)
+    bot.send_message(my_chat, t)
 
-def execuse_smth(m, code):
+def execuse_smth(message, code):
     """
     Report user about problem
-    Transfer control to choose_category (next handler)
 
     """
     text = code_wrongs[code]
-    bot.send_message(m.chat.id, text)
-    msg = invalidate(m, const.invite_cat, categories,0)
-    bot.register_next_step_handler(msg, choose_category)
+    bot.send_message(message.chat.id, text)
     return
 
 def deploy():
 
     """WEBHOOKS SET UP:"""
-    WEBHOOK_URL_PATH = "/%s/" % tokens.token_main
+    WEBHOOK_URL_PATH = "/%s/" % tokens.token_main #!!!
     bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
                     certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
--- const.py
+++ const.py
@@ -3,47 +3,40 @@
 import gettext
 gettext.install('const', './lang')
 
-invite_cat =_('''
+INVITE_CATEGORY =_('''
         Choose the category
         ''')
-invite_sc = _('''
+INVITE_SUBCATEGORY = _('''
         Choose the subcategory
         ''')
-smth_wrong = _('''
+SMTH_WRONG = _('''
         Something go bad
         Now we will fix everything; \
-        try again later or choose other service!
+        try again later or choose another service!
         ''')
-my_chat = 254282848
 
+superusers = [] #!
 
-superusers = [my_chat,] #!
-
-sc_is_empty = _('''
+SC_IS_EMPTY = _('''
         There are no companies in this subcategory yet
         You can choose other service
         ''')
-no_category=_('''
+NO_CATEGORY=_('''
         We haven't such category yet
         But you have an access to all the other!
         ''')
-no_subcategory=_('''
+NO_SUBCATEGORY=_('''
         We haven't such subcategory yet
         But you have an access to all the other!
         ''')
 
-unique_users = _("Number of unique users: %d\n")
-unique_clicks = _("Number of unique clicks on all bots: %d")
-
-back = "⇐ back"
+UNIQUE_USERS = _("Number of unique users: %d\n")
+UNIQUE_CLICKS = _("Number of unique clicks on all bots: %d")
 
 FAQ = _('''
     Command list:
     /start - back to choosing categories
     ''')
-available_commands = ("/start", "/get")
-
-
 
 WEBHOOK_HOST = '194.87.0.0'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port has to be open!)
